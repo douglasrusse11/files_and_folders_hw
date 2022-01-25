e042d1bba111d8b4b9557312e668dc0e4dfd9cfd
Douglas Russell <douglas.russell7@btinternet.com>
2022-01-25 18:26:44 +0000

    first commit

---
+++ src/test/java/com/codeclan/example/files_and_folders_hw/FilesAndFoldersHwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.files_and_folders_hw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilesAndFoldersHwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
